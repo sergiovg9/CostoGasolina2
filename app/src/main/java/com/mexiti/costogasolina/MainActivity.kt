2b8fe13b860de204b7c6ed2cf5c8eb0209913532
sergiovg9 <chekovg@hotmail.com>
2024-02-19 17:51:23 -0600

    Edición 1

--- app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
+++ app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.mexiti.costogasolina
 
 import android.os.Bundle
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.mexiti.costogasolina.ui.theme.CostoGasolinaTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +42,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.LightGray
                 ) {
-                    CostGasLayout("Android")
+                    CostGasLayout()
                 }
             }
         }
@@ -41,11 +52,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CostGasLayout(name: String) {
-    Column {
+fun CostGasLayout() {
+    var precioLitro by remember{
+        mutableStateOf("")
+    }
+    var cantLitros by remember {
+        mutableStateOf("")
+    }
+    var propina by remember {
+        mutableStateOf("")
+    }
+    var estadoSwitch by remember {
+        mutableStateOf(false)
+    }
+
+    val precio=precioLitro.toDoubleOrNull()?:0.0
+    val cantidad=cantLitros.toDoubleOrNull()?:0.0
+    val tip=propina.toDoubleOrNull()?:0.0
+    val total=CalcularMonto(precio,cantidad, tip)
+
+    Column (
+        modifier=Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ){
         Text(
             text = stringResource(R.string.calcular_monto),
-
+            fontWeight = FontWeight.Bold
             )
        EditNumberField(
            label = R.string.ingresa_gasolina,
@@ -54,27 +88,65 @@ fun CostGasLayout(name: String) {
                keyboardType = KeyboardType.Number,
                imeAction = ImeAction.Next
            ),
-           value = "10.0",
-           onValueChanged = {}
+           value = precioLitro,
+           onValueChanged = {precioLitro=it},
+           modifier=Modifier.fillMaxWidth()
        )
-        TextField(
-            value = stringResource(R.string.litros),
-            onValueChange = {}
+        EditNumberField(
+            label = R.string.ingresa_cantidad,
+            leadingIcon = R.drawable.cant_gas ,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = cantLitros,
+            onValueChanged = {cantLitros=it},
+            modifier=Modifier.fillMaxWidth()
         )
-        TextField(
-            value = stringResource(R.string.propina) ,
-            onValueChange = {}
-        )
-        Switch(
-            checked = false,
-            onCheckedChange =  {}
-        )
+
+        if(estadoSwitch){
+            EditNumberField(
+                label = R.string.propina,
+                leadingIcon = R.drawable.money_tip,
+                keyboardsOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                value = propina,
+                onValueChanged = {propina = it},
+                modifier = Modifier.fillMaxWidth()
+            )
+
+        }
+
+
+        Row{
+
+            Text(
+                text = "¿Desea agregar la propina?",
+                textAlign = TextAlign.Center
+            )
+
+            Switch(
+                checked = estadoSwitch,
+                onCheckedChange =  {isChecked ->
+                    estadoSwitch = isChecked
+                }
+            )
+        }
+
         Text(
-            text = "Total:  $"
+            text = "Total:  $total",
+            fontWeight = FontWeight.Bold
         )
 
     }
 
+}
+private fun CalcularMonto(precio:Double,cantLitros:Double,propina:Double):String{
+    val total=(precio*cantLitros)+propina
+
+    return NumberFormat.getCurrencyInstance().format(total)
 }
 
 @Composable
@@ -102,6 +174,6 @@ fun EditNumberField(
 @Composable
 fun CostGasLayoutPreview() {
     CostoGasolinaTheme {
-        CostGasLayout("Android")
+        CostGasLayout()
     }
 }
